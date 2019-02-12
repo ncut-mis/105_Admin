211a52d04267857e3c6e47cd264057901c612be8
3A532055 <ylhc214044@gmail.com>
2019-02-13 02:06:53 +0800

    系統使用者公告相關路徑

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,16 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// 後台
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('/', ['as' => 'admin.dashboard.index', 'uses' => 'AdminDashboardController@index']);
+
+    Route::get('posts'          , ['as' => 'admin.posts.index' , 'uses' => 'AdminPostsController@index']);
+    Route::get('posts/create'   , ['as' => 'admin.posts.create', 'uses' => 'AdminPostsController@create']);
+    Route::get('posts/{id}/edit', ['as' => 'admin.posts.edit'  , 'uses' => 'AdminPostsController@edit']);
+
+    Route::post('posts',          ['as' => 'admin.posts.store', 'uses' => 'AdminPostsController@store']);
+    Route::patch('posts/{id}',    ['as' => 'admin.posts.update', 'uses' => 'AdminPostsController@update']);
+    Route::delete('posts/{id}',    ['as' => 'admin.posts.destroy', 'uses' => 'AdminPostsController@destroy']);
+});
