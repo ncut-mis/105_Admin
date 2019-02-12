9f8f2f0ca2835fb16c1fc5435dd4c47d9d9290e6
3A532055 <ylhc214044@gmail.com>
2019-02-13 02:22:08 +0800

    後台主頁與管理者公告頁面的Controller

---
+++ app/Http/Controllers/AdminPostsController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests\PostRequest;
+use App\Post;
+
+class AdminPostsController extends Controller
+{
+    public function index()
+    {
+        $posts=Post::orderBy('created_at','DESC')->get();
+        $data=['posts'=>$posts];
+        return view('admin.posts.index',$data);
+    }
+
+    public function create()
+    {
+        return view('admin.posts.create');
+    }
+
+    public function edit($id)
+    {
+        $post = Post::find($id);
+        $data = ['post' => $post];
+
+        return view('admin.posts.edit', $data);
+    }
+
+    public function store(PostRequest $request)
+    {
+        Post::create($request‐>all());
+        return redirect()->route('admin.posts.index');
+    }
+
+    public function destroy($id)
+    {
+        Post::destroy($id);
+        return redirect()->route('admin.posts.index');
+    }
+
+    public function update(PostRequest $request, $id)
+    {
+        $post=Post::find($id);
+        $post->update($request->all());
+        return redirect()->route('admin.posts.index');
+    }
+
+
+}
