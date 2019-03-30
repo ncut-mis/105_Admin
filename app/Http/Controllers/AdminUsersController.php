94a0d4920eb4281ccbd96125cd0762b645df490a
3A532085 <Luxgen077@gmail.com>
2019-03-30 22:45:14 +0800

    修改AdminUserController的index()&create()&store()方法

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 
+use App\Clinic;
 use App\Http\Requests\UserRequest;
+use Illuminate\Http\Request;
+use App\Staff;
 use App\User;
 
 class AdminUsersController extends Controller
@@ -11,15 +14,19 @@ class AdminUsersController extends Controller
 
     public function index()
     {
-        $users =User::orderBy('created_at','DESC')->get();
-        $data=['users'=>$users];
+        $users =User::orderBy('id','ASC')->get();
+        $clinics =Clinic::orderBy('id','ASC')->get();
+        $staffs =Staff::orderBy('id','ASC')->get();
+        $data=['users'=>$users,'clinics'=>$clinics,'staffs'=>$staffs];
         return view('admin.users.index',$data);
     }
 
 
     protected function create()
     {
-        return view('admin.users.create');
+        $clinics =Clinic::orderBy('id','DESC')->get();
+        $data=['clinics'=>$clinics];
+        return view('admin.users.create',$data);
     }
 
     public function edit($id)
@@ -30,9 +37,19 @@ class AdminUsersController extends Controller
         return view('admin.users.edit', $data);
     }
 
-    public function store(UserRequest $request)
+    public function store(Request $request)
     {
-        User::create($request->all());
+        Clinic::create([
+            'name' => $request->clinic_name,
+        ]);
+
+        Staff::create([
+            'clinic_id' =>$request->clinic_no,
+            'position_id' => 1,
+            'name' => $request->clinic_admin,
+            'email' => $request->email,
+            'password' => $request->password,
+        ]);
         return redirect()->route('admin.users.index');
     }
 
